063161838171676894a4c072a97ee72c0d7b66dd
Cappu123 <cappyalemayehu12@gmail.com>
2024-06-09 23:47:15 -0700

    endpoint to count all objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ Creates Flask app views
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 @app_views.route('/status')
 def app_status():
@@ -12,3 +13,17 @@ def app_status():
     """
     response = {'status': "OK"}
     return jsonify(response)
+@app_views.route('/stats')
+def get__tats():
+    """
+    """
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User'),
+        }
+    return jsonify(stats)
+
