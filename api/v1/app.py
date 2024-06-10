ad8c8211ffcb7398a32c455c5a9262c2704f0f93
Cappu123 <cappyalemayehu12@gmail.com>
2024-06-10 00:28:49 -0700

    for not found design

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ Create a new Flask app, and register the blue print "app_views" to Flask instanc
 '''
 
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -13,8 +13,23 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+@app.teardown_appcontext
+def teardown_engine(exception):
+    """
+
+    """
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+
+    """
+    response ={"error": "Not found"}
+    return jsonify(response), 404
+
 
 if __name__ == '__main__':
     HOST = getenv('HBNB_API_HOST', '0,0,0,0')
     PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=HOST, port= PORT, threaded=True)
+    app.run(debug=True, host=HOST, port= PORT, threaded=True)
