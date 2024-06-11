aaa35494be68a0fb7025b44bac5c8a1d7a3fb2e5
Cappu123 <cappyalemayehu12@gmail.com>
2024-06-11 10:53:35 -0700

    for city

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -33,7 +33,7 @@ def get_city(city_id):
     else:
         return abort(404)
 
-@app_views.route('cities/<city_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
     """
     Retrieves a City
@@ -45,3 +45,66 @@ def delete_city(city_id):
         return jsonify({}), 200
     else:
         return abort(404)
+
+@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+def create_city(state_id):
+    """
+    creates a city
+    """
+    if request.content_type != 'application/json':
+        return abort(400, 'Not a JSON')
+    state = storage.get(State, state_id)
+    if not state:
+        return abort(404)
+    if not request.get_json():
+        return abort(400, 'Not a JSON')
+    data = request.get_json()
+    if 'name' not in data:
+        return abort(400, 'Missing name')
+    data['state_id'] = state_id
+
+    city = City(**data)
+    city.save()
+
+    return jsonify(city.to_dict()), 201
+
+@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
+def update_city(city_id):
+    """
+    updates a City
+    """
+    if request.content_type != 'application/json':
+        return abort(400, 'Not a JSON')
+    city = storage.get(City, city_id)
+    if city:
+        if request.get_json():
+            return abort(400, 'Not a JSON')
+
+        data = request.get_json()
+        ignore_keys = ['id', 'state_id', 'created_at', 'updated_at']
+
+        for key, value in data.items():
+            if key not in ignore_keys:
+                setattr(city, key, value)
+        city.save()
+        return jsonify(city.to_dict()), 200
+    else:
+        return abort(404)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
