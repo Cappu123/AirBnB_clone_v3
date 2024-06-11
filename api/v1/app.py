38c05c5366346c7704fb363e071fb05f92a10230
Cappu123 <cappyalemayehu12@gmail.com>
2024-06-11 15:41:39 -0700

    cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''
-Create a new Flask app, and register the blue print "app_views" to Flask instance app/
+Create a new Flask app, and
+register the blue print "app_views" to Flask instance app/
 '''
 
 from os import getenv
@@ -15,6 +16,7 @@ app = Flask(__name__)
 CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_engine(exception):
     """
@@ -22,16 +24,17 @@ def teardown_engine(exception):
     """
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     """
 
     """
-    response ={"error": "Not found"}
+    response = {"error": "Not found"}
     return jsonify(response), 404
 
 
 if __name__ == '__main__':
     HOST = getenv('HBNB_API_HOST', '0,0,0,0')
     PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(debug=True, host=HOST, port= PORT, threaded=True)
+    app.run(debug=True, host=HOST, port=PORT, threaded=True)
